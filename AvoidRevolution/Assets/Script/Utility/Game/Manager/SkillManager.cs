1e5a25905eeae122cb60764f5c93ad102a8d1207
White-Wolf <cihi13042@g.nihon-u.ac.jp>
2016-06-07 16:09:42 +0900

    ベースシステムの設置

---
+++ AvoidRevolution/Assets/Script/Utility/Game/Manager/SkillManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using AvoidRevolution.Utility;
+
+namespace AvoidRevolution.Utility.Game.Manager
+{
+	/// <summary>
+	/// スキル設定の構造体
+	/// </summary>
+	[Serializable]
+	public struct SKILL_SETTING
+	{
+		// 検索・設定用のスキルID(英字)
+		public string idName;
+
+		// 表示用のスキル名
+		public string name;
+
+		// 表示用のスキル説明
+		public string explanationText;
+
+		// スキルオブジェクト
+		public GameObject SkillObject;
+	}
+
+	/// <summary>
+	/// スキルを管理するクラス
+	/// </summary>
+	public class SkillManager : SingletonMonoBehaviour<SkillManager> 
+	{
+		#region フィールド/プロパティ
+
+		/// <summary>
+		/// スキルのリスト
+		/// </summary>
+		[SerializeField]
+		private List<SKILL_SETTING> SkillList = new List<SKILL_SETTING>();
+
+		#endregion
+
+		#region メソッド
+
+		#endregion
+	}
+}
