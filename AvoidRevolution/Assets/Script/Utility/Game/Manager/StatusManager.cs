9d3fdb8e2981a6c7a959c739ba18ca8d88d893a5
White-Wolf <cihi13042@g.nihon-u.ac.jp>
2016-06-07 17:21:16 +0900

    ステータスを管理するマネージャーの用意

---
+++ AvoidRevolution/Assets/Script/Utility/Game/Manager/StatusManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AvoidRevolution.Utility.Game.Manager
+{
+	/// <summary>
+	/// ステータス設定の構造体
+	/// </summary>
+	[Serializable]
+	public struct STATUS_SETTINGS
+	{
+		// HP
+		public float statusHp;
+
+		// Attack
+		public float statusAttack;
+	}
+
+	/// <summary>
+	/// ステータスを管理するクラス
+	/// </summary>
+	public class StatusManager : SingletonMonoBehaviour<StatusManager> 
+	{
+	}
+}
