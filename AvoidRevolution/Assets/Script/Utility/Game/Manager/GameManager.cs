63483a0c3cb8157c946689e61081b55c442533a8
White-Wolf <cihi13042@g.nihon-u.ac.jp>
2016-09-13 07:52:00 +0900

    最新アップロード

--- AvoidRevolution/Assets/Script/Utility/Game/Manager/GameManager.cs
+++ AvoidRevolution/Assets/Script/Utility/Game/Manager/GameManager.cs
@@ -1,79 +1,161 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using AvoidRevolution.Utility.Game.PlayerSystem;
 
 namespace AvoidRevolution.Utility.Game.Manager
 {
-	/// <summary>
-	/// ゲームを管理するメインのクラス
-	/// </summary>
-	public class GameManager : SingletonMonoBehaviour<GameManager> {
+    // キャラクタのベースとなる構造体
+    [Serializable]
+    public struct CHARACTER_BASE
+    {
+        // プレイヤID
+        public int playerId;
+    }
 
-		#region フィールド/プロパティ
+    /// <summary>
+    /// ゲームを管理するメインのクラス
+    /// </summary>
+    public class GameManager : SingletonMonoBehaviour<GameManager>
+    {
+        #region フィールド/プロパティ
 
-		/// <summary>
-		/// プレイヤ
-		/// </summary>
-		[SerializeField]
-		private GameObject playerObj;
+        /// <summary>
+        /// 選択されたキャラクタのベースデータ
+        /// </summary>
+        public CHARACTER_BASE selectCharData;
 
-		/// <summary>
-		/// プレイヤのID(Test:1111)
-		/// </summary>
-		[SerializeField]
-		private int playerId;
+        /// <summary>
+        /// プレイヤのオブジェクト
+        /// </summary>
+        [SerializeField]
+        private GameObject playerObj;
 
-		/// <summary>
-		/// スコア
-		/// </summary>
-		private int score;
+        /// <summary>
+        /// プレイヤのデータ
+        /// </summary>
+        [SerializeField]
+        private List<CHARACTER_BASE> charDataList = new List<CHARACTER_BASE>();
 
-		/// <summary>
-		/// スコアのアクセサ
-		/// </summary>
-		/// <value>スコア</value>
-		int Score
-		{
-			set
-			{
-				score = value;
-			}
+        /// <summary>
+        /// 初期位置の設定
+        /// </summary>
+        [SerializeField]
+        private List<Vector2> startPosList = new List<Vector2>();
 
-			get
-			{
-				return score;
-			}
-		}
+        /// <summary>
+        /// カラーリスト
+        /// 後で削除
+        /// </summary>
+        private List<Color> colorList = new List<Color>(){ Color.red, Color.blue, Color.green, Color.yellow };
 
-		#endregion
+        /// <summary>
+        /// スコア
+        /// </summary>
+        private int score;
 
-		#region メソッド
+        /// <summary>
+        /// スコアのアクセサ
+        /// </summary>
+        /// <value>スコア</value>
+        int Score
+        {
+            set
+            {
+                score = value;
+            }
 
-		/// <summary>
-		/// コンストラクタ
-		/// </summary>
-		private GameManager()
-		{
-		}
+            get
+            {
+                return score;
+            }
+        }
 
-		/// <summary>
-		/// 一番最初に処理する
-		/// </summary>
-		void Awake()
-		{
-			// プレイヤの設定
-			SetPlayerObjects ();
-		}
+        #endregion
 
-		/// <summary>
-		/// プレイヤの設定
-		/// </summary>
-		private void SetPlayerObjects()
-		{
-			// オフラインの場合
-			GameObject obj = Instantiate(playerObj);
-			obj.name = "Player_" + playerId.ToString();
-		}
+        #region メソッド
 
-		#endregion
-	}
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        private GameManager()
+        {
+			
+        }
+
+        void Awake()
+        {
+            // プレイヤの設定
+            SetPlayerObjects();
+        }
+
+        /// <summary>
+        /// 毎フレームごと呼ぶ
+        /// 後で削除
+        /// </summary>
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                // player1
+                SelectPlayer(charDataList[0].playerId);
+            }
+            else if (Input.GetKeyDown(KeyCode.X))
+            {
+                // player2
+                SelectPlayer(charDataList[1].playerId);
+            }
+            else if (Input.GetKeyDown(KeyCode.C))
+            {
+                // player3
+                SelectPlayer(charDataList[2].playerId);
+            }
+            else if (Input.GetKeyDown(KeyCode.V))
+            {
+                // player4
+                SelectPlayer(charDataList[3].playerId);
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SelectPlayer();
+            }
+        }
+
+        /// <summary>
+        /// 操作キャラを選択する
+        /// </summary>
+        /// <param name="uniqueId">ユニークID(-1:何もない状態)</param>
+        public void SelectPlayer(int uniqueId = -1)
+        {
+            foreach (CHARACTER_BASE data in charDataList)
+            {
+                if (uniqueId == data.playerId)
+                {
+                    selectCharData = data;
+                    return;
+                }
+            }
+            selectCharData = new CHARACTER_BASE();
+        }
+
+        /// <summary>
+        /// プレイヤの設定
+        /// </summary>
+        private void SetPlayerObjects()
+        {
+            // オフラインの場合
+            foreach (CHARACTER_BASE data in charDataList)
+            {
+                GameObject obj = Instantiate(playerObj);
+                obj.transform.position = startPosList[charDataList.IndexOf(data)];
+                obj.name = "Player_" + data.playerId;
+                obj.GetComponent<PlayerController>().UniqueId = data.playerId;
+                obj.GetComponent<SpriteRenderer>().color = colorList[charDataList.IndexOf(data)];
+            }
+            SelectPlayer(charDataList[0].playerId);
+        }
+
+        #endregion
+    }
 }
--- AvoidRevolution/Assets/Script/Utility/Game/PlayerSystem/PlayerController.cs
+++ AvoidRevolution/Assets/Script/Utility/Game/PlayerSystem/PlayerController.cs
@@ -1,119 +1,272 @@
 ﻿using UnityEngine;
 using System.Collections;
+using AvoidRevolution.Utility.Game.Manager;
 
 namespace AvoidRevolution.Utility.Game.PlayerSystem
 {
-	/// <summary>
-	/// プレイヤーのコントローラークラス
-	/// </summary>
-	public class PlayerController : MonoBehaviour
-	{
-		#region フィールド/プロパティ
+    /// <summary>
+    /// プレイヤーのコントローラークラス
+    /// </summary>
+    public class PlayerController : MonoBehaviour
+    {
+        #region フィールド/プロパティ
 
-		/// <summary>
-		/// プレイヤーのRigidbody2D
-		/// </summary>
-		private Rigidbody2D playerRig2D;
+        /// <summary>
+        /// プレイヤーのRigidbody2D
+        /// </summary>
+        private Rigidbody2D playerRig2D;
 
-		/// <summary>
-		/// 基礎となる吹き飛ばし力
-		/// </summary>
-		[SerializeField]
-		private float baseForceValue;
+        /// <summary>
+        /// 基礎となる吹き飛ばし力
+        /// </summary>
+        [SerializeField]
+        private float baseForceValue;
 
-		/// <summary>
-		/// 操作可能か
-		/// </summary>
-		private bool isControl;
+        /// <summary>
+        /// 矢印オブジェクト
+        /// </summary>
+        private GameObject arowObject;
 
-		/// <summary>
-		/// 画面を触った時の位置
-		/// </summary>
-		private Vector2 startPos;
+        /// <summary>
+        /// 操作可能か
+        /// </summary>
+        private bool isControl;
 
-		/// <summary>
-		/// 現在の位置
-		/// </summary>
-		private Vector2 nowPos;
+        /// <summary>
+        /// 画面を触った時の位置
+        /// </summary>
+        private Vector2 startPos;
 
-		/// <summary>
-		/// 画面を離れた時の位置
-		/// </summary>
-		private Vector2 endPos;
+        /// <summary>
+        /// 現在の位置
+        /// </summary>
+        private Vector2 nowPos;
 
-		/// <summary>
-		/// addForceで力を加える基礎力
-		/// </summary>
-		private Vector2 forceVec;
+        /// <summary>
+        /// 画面を離れた時の位置
+        /// </summary>
+        private Vector2 endPos;
 
-		#endregion
+        /// <summary>
+        /// addForceで力を加える基礎力
+        /// </summary>
+        private Vector2 forceVec;
 
-		#region メソッド
+        /// <summary>
+        /// 移動時間
+        /// </summary>
+        [SerializeField]
+        private float moveTime;
 
-		/// <summary>
-		/// 一番最初に呼ばれる
-		/// </summary>
-		void Awake()
-		{
-			SetupPlayerController ();
-		}
+        /// <summary>
+        /// Forceの制限
+        /// </summary>
+        [SerializeField]
+        private int limitForce;
 
-		/// <summary>
-		/// 毎フレーム呼ばれる
-		/// </summary>
-		void Update()
-		{
-			if (!isControl)
-				return;
-			//操作
-			Control ();
-		}
+        /// <summary>
+        /// ユニークID
+        /// </summary>
+        private int uniqueId;
 
-		/// <summary>
-		/// プレイヤーコントローラー
-		/// </summary>
-		private void SetupPlayerController()
-		{
-			// プレイヤーのRigidbody2Dを設定
-			playerRig2D = this.GetComponent<Rigidbody2D> ();
-			isControl = true;
-		}
+        /// <summary>
+        /// 減速判断
+        /// </summary>
+        private bool isDeceleration;
 
-		/// <summary>
-		/// キャラクタの操作を行う
-		/// </summary>
-		private void Control()
-		{
-			if (Input.GetMouseButtonDown (0)) {
-				// TODO AvoidRevolution 操作メソッドに変更
-				startPos = GetInputMousePosition();
-			}
-			if (Input.GetMouseButton (0)) {
-				// TODO AvoidRevolution 操作メソッドの変更
-				nowPos = GetInputMousePosition();
-				forceVec = new Vector2 (startPos.x - nowPos.x, startPos.y - nowPos.y);
-			}
-			if (Input.GetMouseButtonUp (0)) {
-				// TODO AvoidRevolution 操作メソッドに変更
-				endPos = GetInputMousePosition();
-				forceVec = new Vector2 (startPos.x - endPos.x, startPos.y - endPos.y);
-				playerRig2D.AddForce (forceVec * baseForceValue * Time.deltaTime);
-			}
-			// TODO AvoidRevolution 描画メソッドの読み込み
+        /// <summary>
+        /// 移動制御タイマー
+        /// </summary>
+        private float fluctuationMoveTime;
 
-			// 減速
-			playerRig2D.velocity *= 0.99f;
-		}
+        /// <summary>
+        /// ユニークIDのアクセサ
+        /// </summary>
+        /// <value>ユニークID</value>
+        public int UniqueId
+        {
+            set
+            {
+                uniqueId = value;
+            }
+        }
 
-		/// <summary>
-		/// タップした位置の取得
-		/// </summary>
-		/// <returns>タップした位置</returns>
-		private Vector2 GetInputMousePosition()
-		{
-			return Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		}
+        #endregion
 
-		#endregion
-	}
+        #region メソッド
+
+        /// <summary>
+        /// 一番最初に呼ばれる
+        /// </summary>
+        void Awake()
+        {
+            SetupPlayerController();
+        }
+
+        /// <summary>
+        /// 毎フレーム呼ばれる
+        /// </summary>
+        void Update()
+        {
+            move();
+            if (!isControl || !IsSelectPlayer())
+                return;
+            //操作
+            Control();
+        }
+
+        /// <summary>
+        /// 自分自身が操作キャラクタとして選択されているかどうか
+        /// </summary>
+        /// <returns>操作キャラクタかどうか</returns>
+        private bool IsSelectPlayer()
+        {
+            if (uniqueId == GameManager.Instance.selectCharData.playerId)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// プレイヤーコントローラー
+        /// </summary>
+        private void SetupPlayerController()
+        {
+            // プレイヤーのRigidbody2Dを設定
+            playerRig2D = this.GetComponent<Rigidbody2D>();
+            isControl = true;
+            isDeceleration = true;
+            // TODO AvoidRevolution 矢印オブジェクトの初期化
+        }
+
+        private void moveTimer()
+        {
+            if (!isDeceleration)
+            {
+                fluctuationMoveTime -= Time.deltaTime;
+                if (fluctuationMoveTime <= 0)
+                {
+                    isDeceleration = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 位置の固定
+        /// </summary>
+        private void FreezePosition()
+        {
+            playerRig2D.constraints = RigidbodyConstraints2D.FreezePosition;
+        }
+
+        /// <summary>
+        /// 位置の固定を解除
+        /// </summary>
+        private void CancellationFreezePosition()
+        {
+            playerRig2D.constraints = RigidbodyConstraints2D.None;
+        }
+
+        /// <summary>
+        /// キャラクタの動作を制御する
+        /// </summary>
+        private void move()
+        {
+            if (playerRig2D.velocity.magnitude < 1)
+            {
+                isControl = true;
+            }
+            else
+            {
+                isControl = false;
+            }
+            moveTimer();
+            if (isDeceleration)
+            {
+                // 減速
+                playerRig2D.velocity *= 0.98f;
+                if (playerRig2D.velocity.magnitude < 1)
+                {
+                    playerRig2D.velocity = Vector2.zero;
+                    FreezePosition();
+                }
+            }
+        }
+
+        /// <summary>
+        /// キャラクタの操作を行う
+        /// </summary>
+        private void Control()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // TODO AvoidRevolution 操作メソッドに変更
+                startPos = GetInputMousePosition();
+            }
+            if (Input.GetMouseButton(0))
+            {
+                // TODO AvoidRevolution 操作メソッドの変更
+                nowPos = GetInputMousePosition();
+                forceVec = new Vector2(startPos.x - nowPos.x, startPos.y - nowPos.y);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                // TODO AvoidRevolution 操作メソッドに変更
+                endPos = GetInputMousePosition();
+                forceVec = new Vector2(startPos.x - endPos.x, startPos.y - endPos.y);
+                if (IsAddForce(forceVec))
+                {
+                    CancellationFreezePosition();
+                    playerRig2D.AddForce(forceVec * baseForceValue * Time.deltaTime);
+                }
+                isDeceleration = false;
+                fluctuationMoveTime = moveTime;
+            }
+            // TODO AvoidRevolution 矢印描画メソッドの読み込み
+            DrawingArrow(forceVec);
+        }
+
+        /// <summary>
+        /// タップした位置の取得
+        /// </summary>
+        /// <returns>タップした位置</returns>
+        private Vector2 GetInputMousePosition()
+        {
+            return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        /// <summary>
+        /// AddForce制限の判定
+        /// </summary>
+        /// <returns>AddForce有効か</returns>
+        /// <param name="force">加えるForce</param>
+        private bool IsAddForce(Vector2 force)
+        {
+            if (Mathf.Pow(limitForce, 2) > Mathf.Pow(force.x, 2) + Mathf.Pow(force.y, 2))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// 方向矢印の描画
+        /// </summary>
+        private void DrawingArrow(Vector2 force)
+        {
+            if (!IsAddForce(force))
+                return;
+            // 矢印の描画
+
+        }
+
+        #endregion
+    }
 }
